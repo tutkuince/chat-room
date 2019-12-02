2179e0e5164c9467a30f89d0336d29dd18126cba
tutkuince <tutku.ince@outlook.com>
2019-12-02 12:58:18 +0300

    message model

---
+++ src/main/java/com/muditasoft/chatroom/model/Message.java
@@ -0,0 +1,23 @@
+package com.muditasoft.chatroom.model;
+/**
+ * WebSocket message model
+ */
+public class Message {
+
+    private String content;
+
+    public Message() {
+    }
+
+    public Message(String content) {
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
