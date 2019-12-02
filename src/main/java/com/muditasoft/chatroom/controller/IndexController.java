5ac042102b9da20efecc4cce5c153197898d5c71
tutkuince <tutku.ince@outlook.com>
2019-12-02 13:23:11 +0300

    IndexController

---
+++ src/main/java/com/muditasoft/chatroom/controller/IndexController.java
@@ -0,0 +1,21 @@
+package com.muditasoft.chatroom.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import java.net.UnknownHostException;
+
+@RestController
+public class IndexController {
+
+    /**
+     * Chatroom Page
+     */
+    @GetMapping("/index")
+    public ModelAndView index(String username, HttpServletRequest request) throws UnknownHostException {
+        //TODO: add code for login to chatroom.
+        return null;
+    }
+}
